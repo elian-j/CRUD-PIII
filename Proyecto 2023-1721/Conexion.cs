ef352d46c79927b0708001449328c535addcba20
elian-j <eliansteven648@gmail.com>
2025-04-04 20:33:45 -0400

    Primera version

---
+++ Proyecto 2023-1721/Conexion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Proyecto_2023_1721
+{
+    internal class Conexion
+    {
+        public static SqlConnection ObtenerConexion()
+        {
+            string cadena = ConfigurationManager.ConnectionStrings["cn"].ConnectionString;
+            return new SqlConnection(cadena);
+        }
+    }
+}
